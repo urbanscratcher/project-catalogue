7e32c0caa13cf69d75448fbecb88cd11fe393f27
Joun <urbanscratcher@gmail.com>
2024-01-29 21:17:04 +0900

    hide id

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,16 +1,13 @@
 export const PROXIED_URL: string =
-  "https://notion-cors.";
+  "https://notion-cors.urbanscratcher.workers.dev";
 
-export const DEV_DB: string = "";
-export const PERSONAL_DB: string = "";
-export const PROJECT_DB: string = "";
+export const DEV_DB: string = process.env.NOTION_DEV_DB_ID || "";
+export const PERSONAL_DB: string = process.env.NOTION_PERSONAL_DB_ID || "";
+export const PROJECT_DB: string = process.env.NOTION_PROJECT_DB_ID || "";
 
 export enum MENU {
   ABOUT = "About",
   PROJECTS = "Projects",
-  DEVLOGS = "Devlogs",
-  NOTES = "Notes",
-  PERSONAL = "Personal",
   CONTACT = "Contact",
 }
 
