466e6f36f9c7497dceda3b0094ebca07e4eda4b7
Joun <urbanscratcher@gmail.com>
2024-01-29 21:09:57 +0900

    Update constants.ts
    removed sensitive data

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,9 +1,9 @@
 export const PROXIED_URL: string =
-  "https://notion-cors.urbanscratcher.workers.dev";
+  "https://notion-cors";
 
-export const DEV_DB: string = "e801ce3333894b8289a9da590995c8c8";
-export const PERSONAL_DB: string = "edd436f6430c4c19b7090e3e136c3be2";
-export const PROJECT_DB: string = "abebd9aa39224ad49bef2a52d9f73d87";
+export const DEV_DB: string = "";
+export const PERSONAL_DB: string = "";
+export const PROJECT_DB: string = "";
 
 export enum MENU {
   ABOUT = "About",
